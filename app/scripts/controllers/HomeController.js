1e26a7b16c6c8c79a32a5dbcea64be2921f3f188
fishermanswharff <fishermanswharff@github.com>
2014-12-09 13:21:27 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -2,4 +2,11 @@
 angular.module('MainController').controller('HomeController',function($scope,PostsFactory,trace){
   $scope.posts = PostsFactory.posts;
   
+  $scope.hasImage = function(post){
+    var posts = [];
+    posts = $scope.posts.filter(function(item){
+      return item.image != "/images/original/missing.png"
+    });
+    return posts.length > 0;
+  };
 });
