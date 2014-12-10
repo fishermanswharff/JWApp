4e9396300cac4aa11f6903be6e96b2864e88b933
fishermanswharff <fishermanswharff@github.com>
2014-12-10 16:59:54 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/FooterController.js
+++ app/scripts/controllers/FooterController.js
@@ -1,5 +1,6 @@
 'use strict';
-angular.module('MainController').controller('NavbarController',function($scope,trace){
+
+angular.module('MainController').controller('FooterController',function($scope){
   $scope.accounts = [
     {
       'method': 'Email',
@@ -32,35 +33,4 @@ angular.module('MainController').controller('NavbarController',function($scope,t
       'icon': 'images/icons/tumblr-white.svg'
     }
   ];
-
-  // trace($scope.accounts);
 });
-
-/*
-'contact':{
-    'Email': {
-      'url': 'mailto:fishermanswharff@mac.com',
-      'icon': 'images/email-white.svg'
-    },
-    'Github': {
-      'url': 'https://github.com/fishermanswharff',
-      'icon': 'images/github-white.svg'
-    },
-    'Twitter': {
-      'url': 'https://twitter.com/jasonwharff',
-      'icon': 'images/twitter-white.svg'
-    },
-    'LinkedIn': {
-      'url': 'https://www.linkedin.com/in/jasonwharff',
-      'icon': 'images/linkedin-white.svg'
-    },
-    'Instagram': {
-      'url': 'http://instagram.com/jasonwharff',
-      'icon': 'images/instagram-white.svg'
-    },
-    'Tumblr': {
-      'url': 'http://blog.jasonwharff.com',
-      'icon': 'images/tumblr-white.svg'
-    }
-
-*/
