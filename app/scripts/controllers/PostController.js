9792a8502ce9f9ea524102d3ebfd419f159eb180
fishermanswharff <fishermanswharff@github.com>
2014-12-11 16:41:31 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/PostController.js
+++ app/scripts/controllers/PostController.js
@@ -1,10 +1,33 @@
 'use strict';
-angular.module('MainController').controller('PostController',function($scope,$http,AuthFactory,AmazonBucket,AWSFactory,CategoryFactory,ServerUrl,trace){
+angular.module('MainController').controller('PostController',function($scope,$q,$http,AuthFactory,AmazonBucket,AWSFactory,CategoryFactory,ServerUrl,trace){
 
   $scope.categories = CategoryFactory.categories;
+  
+  var updateCategories = function(postId){
+    var promises = [];
+    _.forEach($scope.categories,function(item){
+      var isChecked = item.checked;
+      if(isChecked){
+        promises.push($http.put(ServerUrl+'posts/'+postId+'/categories/' + item.id))
+      }
+    });
+  };
+
+  $scope.upsertPost = function(post){
+    var params = { post: post }
+    $http.post(ServerUrl + 'posts',params).success(function(response){
+      $q.all(updateCategories(response.id)).then(function(){
+        $location.path('/posts/'+response.id);
+      });
+    });
+  };
 
   $scope.isLoggedIn = function(){
     return AuthFactory.isAuthenticated();
+  };
+
+  var clearForm = function(){
+
   };
 });
 
