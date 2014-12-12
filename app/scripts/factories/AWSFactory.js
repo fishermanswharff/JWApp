c62f45b003de5b1960c45369373dbbfbcb1b0e0a
fishermanswharff <fishermanswharff@github.com>
2014-12-11 21:39:35 -0500

    Autopush JW Webapp updates

--- app/scripts/factories/AWSFactory.js
+++ app/scripts/factories/AWSFactory.js
@@ -1,19 +1,20 @@
-// .constant('AmazonBucket','http://dubya-blog-bucket.s3.amazonaws.com/');
-
 'use strict';
 angular.module('jwwebApp').factory('AWSFactory',function($http,$q,ServerUrl,AmazonBucket,trace){
-  
+  // .constant('AmazonBucket','http://dubya-blog-bucket.s3.amazonaws.com/');  
   var signKeyResults, imageData, imagePayload;
 
   var prepareImage = function(imageFile, postId){
     return $http.get(ServerUrl + 'amazon/sign_key').success(function(response){
       $q.all(buildKey(response)).then(function(){
-        
+        $http.post(ServerUrl+'posts/'+postId+'/images', imagePayload).success(function(response){
+          debugger;
+          // imageData.append("Policy",signKeyResults.policy);
+        }).error(function(){});
       });
     });
   };
 
-  var buildKey(response){
+  var buildKey = function(response){
     signKeyResults = response;
     imageData = new FormData();
     imagePayload = { image: 
