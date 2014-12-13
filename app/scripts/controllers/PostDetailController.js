6eacbdc67d65d48b881b820294d25d06f4c629f4
fishermanswharff <fishermanswharff@github.com>
2014-12-13 17:05:44 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/PostDetailController.js
+++ app/scripts/controllers/PostDetailController.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('MainController').controller('PostDetailController',function($scope,$http,trace,$window,$routeParams,ServerUrl,AuthFactory){
+angular.module('MainController').controller('PostDetailController',function($scope,$q,$http,$routeParams,$location,AuthFactory,AmazonBucket,AWSFactory,CategoryFactory,ServerUrl,trace){
   
   $http.get(ServerUrl + 'posts/' + $routeParams.postId.toString()).success(function(response){
     $scope.post = response;
@@ -15,6 +15,36 @@ angular.module('MainController').controller('PostDetailController',function($sco
       });
     }
   };
+
+  /*
+  var updateCategories = function(postId){
+    var promises = [];
+    _.forEach($scope.categories,function(item){
+      var isChecked = item.checked;
+      if(isChecked){
+        promises.push($http.put(ServerUrl+'posts/'+postId+'/categories/' + item.id))
+      }
+    });
+  };
+
+  var updateImages = function(postId){
+    var promises = [];
+    var fileInputs = $('#imageUpload > input[type="file"]');
+    for (var i = 0, length = fileInputs.length; i < length; i++) {
+      var imageFile = fileInputs[i].files[0];
+      promises.push(AWSFactory.prepareKey(imageFile,postId));
+    }
+  };
+
+  $scope.upsertPost = function(post){
+    var params = { post: post }
+    $http.post(ServerUrl + 'posts',params).success(function(response){
+      $q.all(updateImages(response.id), updateCategories(response.id)).then(function(){
+        $location.path('/posts/'+response.id);
+      });
+    });
+  };
+  */
 
   $scope.isLoggedIn = function(){
     return AuthFactory.isAuthenticated();
