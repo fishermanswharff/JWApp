df81255517dbd2815d46ff296f8bc9277101bcf0
fishermanswharff <fishermanswharff@github.com>
2014-12-15 12:52:02 -0500

    Autopush JW Webapp updates

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -49,7 +49,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['nyan','coverage'],
+    reporters: [
+      'nyan',
+      'coverage',
+      coverageReporter = {
+        type: 'lcovonly',
+        dir: 'coverage/'
+      }
+    ],
 
 
     // web server port
