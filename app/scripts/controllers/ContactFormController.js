711ee7960380dcb9e792dc1628f5f3d7368a907e
fishermanswharff <fishermanswharff@github.com>
2014-12-16 02:42:32 -0500

    Autopush JW Webapp updates

---
+++ app/scripts/controllers/ContactFormController.js
@@ -0,0 +1,10 @@
+angular.module('MainController').controller('ContactFormController',['$scope','$http','ServerUrl','trace',function($scope,$http,ServerUrl,trace){
+  $scope.message = '';
+  $scope.submitContact = function(message){
+    $http.post(ServerUrl + 'email/admin',{name: message.name, email: message.email, body: message.body}).success(function(){
+      $scope.success = 'Your email was sent successfully.'
+    }).error(function(){
+
+    });
+  };
+}]);
