8d02100dfedfa828a939ca05e8a95d6c60fc2d2a
fishermanswharff <fishermanswharff@github.com>
2014-12-18 18:07:26 -0500

    Autopush JW Webapp updates

--- app/scripts/directives/PostGalleryDirective.js
+++ app/scripts/directives/PostGalleryDirective.js
@@ -7,7 +7,6 @@ angular.module('MainDirective').directive('postGallery',['trace','$timeout',func
     link: function($scope,element,attrs){
       
       var currentPanel,totalPanels,navClicked,distanceToMove,newPhotoPosition,newCaption,photoWidth,images;
-      
       var init = function(){
         totalPanels = $('.slider-photos img').length;
         photoWidth = element.find('div.slider-container').width();
@@ -23,34 +22,42 @@ angular.module('MainDirective').directive('postGallery',['trace','$timeout',func
         $('.slider-photos').css('left','-'+photoWidth+'px');
         currentPanel = $(images).index(images[1]);
         $('.slider-photos').fadeIn(1500);
-        /* debug */ $('.totalPanels').html('totalPanels = '+totalPanels); //.......................
+        $('.totalPanels').html('totalPanels = '+totalPanels);
       };
-
       var resizeImages = function(array,width){
         $(array).each(function(index){
           $(this).css('width',width+'px');
         });
       };
-
       var clickHandler = function(e){
+        var slider = $('.slider-photos');
         $(element).on('click','a',function(e){
+          
           distanceToMove = photoWidth*(-1);
+          var newPhotoPosition; // = (currentPanel*distanceToMove)-photoWidth + 'px';
+          
           if($(this).attr('class') === 'slider-nav prev'){
-            if(currentPanel == 1){
-
+            if(currentPanel === 1){
+              newPhotoPosition = '0px';
+              currentPanel = 0;
+            } else if(currentPanel === 0){
+              newPhotoPosition = (currentPanel*distanceToMove)+photoWidth + 'px';
+            } else {
+              newPhotoPosition = (currentPanel*distanceToMove)+photoWidth + 'px';
             }
-            // trace('previous nav clicked');
+            trace('previous nav clicked');
           } else if ($(this).attr('class') === 'slider-nav next'){
-            if(currentPanel >= images.length - 1){
+            if(currentPanel == totalPanels + 1){
               newPhotoPosition = '0px';
               currentPanel = 0;
             } else {
               newPhotoPosition = (currentPanel*distanceToMove)-photoWidth + 'px'; 
               currentPanel++;
             }
-            $('.slider-photos').animate({left: newPhotoPosition},500);
+            trace('next nav clicked');
           }
-          /* debug */ $('.currentPanel').html('currentPanel = '+currentPanel);  //.....................
+          TweenLite.to(slider,.5,{left: newPhotoPosition});
+          $('.currentPanel').html('currentPanel = '+currentPanel);
         });
       };
       $timeout(function(){
@@ -60,40 +67,3 @@ angular.module('MainDirective').directive('postGallery',['trace','$timeout',func
     }
   };
 }]);
-
-/*
-// Set up Navigation Links
-$('.marquee_nav a.marquee_nav_item').click(function(test){
-  // Set the navigation state
-  $('.marquee_nav a.marquee_nav_item').removeClass('selected');
-  $(this).addClass('selected');
-  var navClicked = $(this).index();
-  var distanceToMove = window.photoWidth*(-1);
-  var newPhotoPosition = (navClicked*distanceToMove)-window.photoWidth + 'px';  //NEW subtract window.width
-  var newCaption = $('.marquee_panel_caption').get(navClicked);
-      
-  // Animate photos
-  if( window.currentPanel == window.totalPanels && navClicked == 0){
-    newPhotoPosition = (window.photoWidth*(window.totalPanels+1)*-1)+'px';
-    $('.marquee_photos').animate({left: newPhotoPosition}, 1000, function(){
-      $('.marquee_photos').css('left','-'+window.photoWidth+'px');
-    });
-  }else if( window.currentPanel == 1 && navClicked == (window.totalPanels-1)){
-    newPhotoPosition = '0px';
-    $('.marquee_photos').animate({left: newPhotoPosition}, 1000, function(){
-      $('.marquee_photos').css('left','-'+(window.photoWidth*window.totalPanels)+'px');
-    });
-  }else{
-    $('.marquee_photos').animate({left: newPhotoPosition}, 1000);
-  }
-  
-  window.currentPanel = navClicked + 1;
-  $('.currentPanel').html('currentPanel = '+window.currentPanel);  //.....................
-  
-  // Animate the caption
-  $('.marquee_caption').animate({top: '340px'}, 500, function(){
-    var newHTML = $(newCaption).html();
-    $('.marquee_caption_content').html(newHTML);
-    setCaption();
-  });
-});
