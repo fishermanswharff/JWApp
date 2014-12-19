1cf1a0dab68e4a3f3388ce5d6bc09f058a01250d
fishermanswharff <fishermanswharff@github.com>
2014-12-19 02:55:11 -0500

    Autopush JW Webapp updates

--- app/scripts/directives/PostGalleryDirective.js
+++ app/scripts/directives/PostGalleryDirective.js
@@ -31,14 +31,14 @@ angular.module('MainDirective').directive('postGallery',['trace','$timeout',func
         var width = Gallery.containerWidth;
         for(var i = 0; i < images.length; i++){
           var photoPosition = i * width;
-          $(images[i]).css('width',width+'px').css('left',photoPosition+'px');
+          $(images[i]).css('width',width+'px').css('left',photoPosition+'px').css('top','0px');
           $('.slider-photos').css('width',photoPosition+width+'px');
         }
       };
 
       Gallery.setCurrentIndex = function(index){
         Gallery.currentIndex = index;
-        if(Gallery.currentIndex > Gallery.images.length) Gallery.currentIndex = 0;
+        if(Gallery.currentIndex > Gallery.images.length - 1) Gallery.currentIndex = 0;
         else if (Gallery.currentIndex < 0) Gallery.currentIndex = Gallery.images.length - 1;
       };
 
@@ -54,79 +54,28 @@ angular.module('MainDirective').directive('postGallery',['trace','$timeout',func
             break;
         }
         Gallery.updateView();
-        trace(Gallery.currentIndex);
       };
 
       Gallery.updateView = function(){
-        // TweenLite.to(Gallery.imgContainer,.5,{left: Gallery.currentIndex * Gallery.containerWidth});
-        // Gallery.images[Gallery.currentIndex])
+        var prevIndex = Gallery.currentIndex - 1;
+        var nextIndex = Gallery.currentIndex + 1;
+        if(prevIndex < 0) prevIndex = Gallery.images.length - 1;
+        if(nextIndex > Gallery.images.length - 1) nextIndex = 0;
+        var tl = new TimelineMax({align: 'start'});
+        tl.add([
+          TweenLite.to(Gallery.images[prevIndex],0.5,{left: "-"+Gallery.containerWidth+'px'}),
+          TweenLite.to(Gallery.images[Gallery.currentIndex], 0.5, {left: '0px'}), 
+          TweenLite.to(Gallery.images[nextIndex],0.5,{left: Gallery.containerWidth+'px'})], 0);
+        Gallery.images.map(function(index){
+          index === Gallery.currentIndex ? $(Gallery.images[index]).css('z-index',1) : $(Gallery.images[index]).css('z-index',0);
+        });
+        $('.currentPanel').html(Gallery.currentIndex);
       };
-      
 
       $timeout(function(){
         Gallery.init();
+        Gallery.updateView();
       },100);
-      
-      /*
-      var currentPanel,totalPanels,navClicked,distanceToMove,newPhotoPosition,newCaption,photoWidth,images;
-      var init = function(){
-        totalPanels = $('.slider-photos img').length;
-        photoWidth = element.find('div.slider-container').width();
-        $('.slider-photos img:last-child').clone().insertBefore('.slider-photos img:first-child');
-        $('.slider-photos img:nth-child(2)').clone().insertAfter('.slider-photos img:last-child');
-        images = $('.slider-photos img');
-        resizeImages(images,photoWidth);
-        $(images).each(function(index){
-          var photoPosition = index * photoWidth;
-          $(this).css('left', photoPosition+'px');
-          $('.slider-photos').css('width',photoPosition+photoWidth+'px');
-        });
-        $('.slider-photos').css('left','-'+photoWidth+'px');
-        currentPanel = $(images).index(images[1]);
-        $('.slider-photos').fadeIn(1500);
-        $('.totalPanels').html('totalPanels = '+totalPanels);
-      };
-      var resizeImages = function(array,width){
-        $(array).each(function(index){
-          $(this).css('width',width+'px');
-        });
-      };
-      var clickHandler = function(e){
-        var slider = $('.slider-photos');
-        $(element).on('click','a',function(e){
-          
-          distanceToMove = photoWidth*(-1);
-          var newPhotoPosition; // = (currentPanel*distanceToMove)-photoWidth + 'px';
-          
-          if($(this).attr('class') === 'slider-nav prev'){
-            if(currentPanel === 1){
-              newPhotoPosition = '0px';
-              currentPanel = 0;
-            } else if(currentPanel === 0){
-              newPhotoPosition = (currentPanel*distanceToMove)+photoWidth + 'px';
-            } else {
-              newPhotoPosition = (currentPanel*distanceToMove)+photoWidth + 'px';
-            }
-            trace('previous nav clicked');
-          } else if ($(this).attr('class') === 'slider-nav next'){
-            if(currentPanel == totalPanels + 1){
-              newPhotoPosition = '0px';
-              currentPanel = 0;
-            } else {
-              newPhotoPosition = (currentPanel*distanceToMove)-photoWidth + 'px'; 
-              currentPanel++;
-            }
-            trace('next nav clicked');
-          }
-          TweenLite.to(slider,.5,{left: newPhotoPosition});
-          $('.currentPanel').html('currentPanel = '+currentPanel);
-        });
-      };
-      $timeout(function(){
-        init();
-        clickHandler();
-      }, 100);
-      */
     }
   };
 }]);
