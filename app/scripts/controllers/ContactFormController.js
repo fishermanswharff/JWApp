0aadbe538d58cea31910f98d72e7ab65412e810e
fishermanswharff <fishermanswharff@github.com>
2014-12-20 13:58:25 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/ContactFormController.js
+++ app/scripts/controllers/ContactFormController.js
@@ -1,11 +1,14 @@
+/*global $:false */
 'use strict';
 angular.module('MainController').controller('ContactFormController',['$scope','$http','ServerUrl','trace',function($scope,$http,ServerUrl,trace){
   $scope.submitContact = function(message){
-    $('#contact-submit-button').addClass('submitted');
+    $('.preloader').addClass('submitted');
+    $('#contact-submit-button').attr('disabled',true);
     $http.post(ServerUrl + 'email/admin',{name: message.name, email: message.email, body: message.body}).success(function(){
       $scope.message = {};
       $scope.success = 'Your email was sent successfully.';
-      $('#contact-submit-button').removeClass('submitted');
+      $('#contact-submit-button').attr('disabled',false);
+      $('.preloader').removeClass('submitted');
     }).error(function(){});
   };
 }]);
