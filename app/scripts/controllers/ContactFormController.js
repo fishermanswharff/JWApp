d3d7dfe4e6d2a8448a6f5c1667431b3bd465a26b
fishermanswharff <fishermanswharff@github.com>
2014-12-20 12:07:12 -0500

    Autopush JW Webapp updates

--- app/scripts/controllers/ContactFormController.js
+++ app/scripts/controllers/ContactFormController.js
@@ -1,5 +1,11 @@
 'use strict';
 angular.module('MainController').controller('ContactFormController',['$scope','$http','ServerUrl','trace',function($scope,$http,ServerUrl,trace){
-  
-  
+  $scope.submitContact = function(message){
+    $('#contact-submit-button').addClass('submitted');
+    $http.post(ServerUrl + 'email/admin',{name: message.name, email: message.email, body: message.body}).success(function(){
+      $scope.message = {};
+      $scope.success = 'Your email was sent successfully.';
+      $('#contact-submit-button').removeClass('submitted');
+    }).error(function(){});
+  };
 }]);
--- app/scripts/directives/ContactFormDirective.js
+++ app/scripts/directives/ContactFormDirective.js
@@ -6,28 +6,6 @@ angular.module('MainDirective').directive('contactForm',['trace','$http','Server
     templateUrl: '../views/contact-form.html',
     link: function($scope,element,attrs){
       
-      $scope.message = '';
-
-      $scope.submitContact = function(message){
-        $('#contact-submit-button').addClass('submitted');
-        $http.post(ServerUrl + 'email/admin',{name: message.name, email: message.email, body: message.body}).success(function(){
-          $q.all(assignScope()).then(function(){
-
-          });
-          
-        }).error(function(){});
-      };
-
-      var assignScope = function(){
-        
-      };
-
-      var clearForm = function(){
-        var promises = [];
-        promises.push();
-        $scope.success = 'Your email was sent successfully.';
-        $('#contact-submit-button').removeClass('submitted');
-      };
     },
   };
 }]);
