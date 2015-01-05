1412d76d8ab7d7e2a5b13fba9b4547fdf41d7d7d
fishermanswharff <fishermanswharff@github.com>
2015-01-05 18:08:35 -0500

    Autopush JW Webapp updates

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,8 +28,7 @@ module.exports = function(config) {
       'test/spec/**/*.js',
     ],
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
