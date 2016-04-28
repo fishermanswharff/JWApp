268489cbe25e817cdbd1a4555413de3103e9d238
Jason Wharff <fishermanswharff@mac.com>
2016-04-28 00:14:26 -0400

    new build, production api url

--- app/scripts/constants/Constants.js
+++ app/scripts/constants/Constants.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('jwwebApp')
   .constant('_', window._)
-  .constant('ServerUrl','http://localhost:3001/')
+  .constant('ServerUrl','https://jwdotcom.herokuapp.com/')
   .constant('AmazonBucket','https://dubya-blog-bucket.s3.amazonaws.com/');
